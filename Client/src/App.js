b71c2625bffbc2b08e6ce0b724d53fdc7598dfd4
Coffey May <jcoffeymay6@gmail.com>
2021-02-06 22:17:29 -0600

    first commit

---
+++ Client/src/App.js
@@ -0,0 +1,17 @@
+import './App.scss';
+import Form from './Components/Form/Form'
+import List from './Components/List/List'
+import Nav from './Components/Nav/Nav'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hry</h1>
+      <Nav />
+      <Form />
+      <List />
+    </div>
+  );
+}
+
+export default App;
