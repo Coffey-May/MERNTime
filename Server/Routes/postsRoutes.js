0801c1dfe0bb832cfc5613adca2985074c8cb6f0
Coffey May <jcoffeymay6@gmail.com>
2021-02-11 11:55:55 -0600

    mongoose

--- Server/Routes/postsRoutes.js
+++ Server/Routes/postsRoutes.js
@@ -1,9 +1,12 @@
 import express from 'express';
-import { getPosts, createPost } from '../Controllers/postsContollers.js'
+import { getPosts, createPost, editPost, deletePost, likePost } from '../Controllers/postsContollers.js'
 
 const router = express.Router();
 
 router.get('/', getPosts);
 router.get('/', createPost);
+router.get('/', updatePost);
+router.get('/', deletePost);
+router.get('/', likePost);
 
 export default router;
