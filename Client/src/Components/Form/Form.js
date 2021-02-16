9d95a0005938f0eabf10747a6c214630f79dc6d8
Coffey May <jcoffeymay6@gmail.com>
2021-02-15 19:36:08 -0600

    form component styling

--- Client/src/Components/Form/Form.js
+++ Client/src/Components/Form/Form.js
@@ -35,30 +35,30 @@ const Form = ({ currentId, setCurrentId }) => {
     };
 
     return (
-        <Tilt>
-            <div className="container">
-                <div className="card">
-                    <div className="content">
-                        <h1>01</h1>
-                        <h3>Card one</h3>
-                        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione obcaecati eius dolorum impedit est! Odio voluptatem quia repellendus eaque debitis.</p>
+        // <Tilt>
+        //     <div className="container">
+        //         <div className="card">
+        //             <div className="content">
+        //                 <h1>01</h1>
+        //                 <h3>Card one</h3>
+        //                 <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ratione obcaecati eius dolorum impedit est! Odio voluptatem quia repellendus eaque debitis.</p>
 
-                    </div>
-                </div>
-            </div>
-        </Tilt>
-        // <Paper className={classes.paper}>
-        //     <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
-        //         <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
-        //         <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
-        //         <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
-        //         <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
-        //         <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
-        //         <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
-        //         <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
-        //         <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
-        //     </form>
-        // </Paper>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </Tilt>
+        <Paper className={classes.paper}>
+            <form autoComplete="off" noValidate className={`${classes.root} ${classes.form}`} onSubmit={handleSubmit}>
+                <Typography variant="h6">{currentId ? `Editing "${post.title}"` : 'Creating a Memory'}</Typography>
+                <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+                <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.title} onChange={(e) => setPostData({ ...postData, title: e.target.value })} />
+                <TextField name="message" variant="outlined" label="Message" fullWidth multiline rows={4} value={postData.message} onChange={(e) => setPostData({ ...postData, message: e.target.value })} />
+                <TextField name="tags" variant="outlined" label="Tags (coma separated)" fullWidth value={postData.tags} onChange={(e) => setPostData({ ...postData, tags: e.target.value.split(',') })} />
+                <div className={classes.fileInput}><FileBase type="file" multiple={false} onDone={({ base64 }) => setPostData({ ...postData, selectedFile: base64 })} /></div>
+                <Button className={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+                <Button variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+            </form>
+        </Paper>
     );
 };
 
