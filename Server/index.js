567fbd087d0e4211325a777891edb51f87e4c574
Coffey May <jcoffeymay6@gmail.com>
2021-02-22 14:26:45 -0600

    likes

--- Server/index.js
+++ Server/index.js
@@ -6,6 +6,7 @@ import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './Routes/postsRoutes.js';
+import userRouter from "./routes/user.js";
 
 const app = express();
 
@@ -14,6 +15,7 @@ app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
+app.use("/user", userRouter);
 
 
 const CONNECTION_URL = 'mongodb+srv://coffeytime:coffeytime123@cluster0.cdxer.mongodb.net/<dbname>?retryWrites=true&w=majority'
