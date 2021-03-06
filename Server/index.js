ba4a07673657b122194617a13c3c885bfd969901
Coffey May <jcoffeymay6@gmail.com>
2021-03-06 10:36:00 -0600

    deployment commit

--- Server/index.js
+++ Server/index.js
@@ -16,17 +16,17 @@ app.use(cors());
 app.use('/posts', postRoutes);
 app.use("/user", userRouter);
 
-const CONNECTION_URL = process.env.CONNECTION_URL
+const CONNECTION_url = process.env.CONNECTION_URL
 // const CONNECTION_url = 'mongodb://coffeytime:coffeytime123@coffey-may-coffey-may-mongo-production/admin'
 const PORT = process.env.PORT || 5000;
 
-// mongoose.connect(CONNECTION_url, { useNewUrlParser: true, useUnifiedTopology: true })
-//     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
-//     .catch((error) => console.log(`${error} did not connect`));
-
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_url, { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
     .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.connect(process.env.CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//     .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+//     .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set('useFindAndModify', false);
 
